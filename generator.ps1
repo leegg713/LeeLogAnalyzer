29e54192e0785dc7bc0b4c734adb63a240b17bd8
Lee Merigold <leemerigold7@gmail.com>
2025-11-19 20:27:37 +0000

    Testing that the import works from config file

--- generator.ps1
+++ generator.ps1
@@ -1,2 +1,6 @@
 # Imports configuration file config.ps1
 . "$PSScriptRoot\config.ps1"
+Write-Host "Test to see if this is working"
+# Gets the log path from config file -- So it basically makes the variable a global one in a way
+$LogPath 
+Write-Host "$LogPath"
